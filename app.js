a55196d6eda1e6c1b919db3a782a273adbb004aa
NelsonRodriguezF <nelmman@hotmail.com>
2021-12-17 17:14:06 -0500

    Agregando archivos de MercadoLiebre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicpath = path.resolve(__dirname, "./public");
+app.use(express.static(publicpath));
+
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.get("/home", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+
+app.listen(process.env.PORT || 3000, () => console.log("Conexion exitosa"));
