be024e391b33f357f64273160e13e7132c596867
NelsonRodriguezF <nelmman@hotmail.com>
2021-12-19 15:17:38 -0500

    Modificando el app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const publicpath = path.resolve(__dirname, "./public");
 app.use(express.static(publicpath));
 
+app.listen(process.env.PORT || 3000, () => console.log("Conexion exitosa"));
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
 app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
 app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
 app.get("/home", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
 
-app.listen(process.env.PORT || 3000, () => console.log("Conexion exitosa"));
